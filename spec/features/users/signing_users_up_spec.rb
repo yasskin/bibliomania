67f9eead34dcc26a7b7d2e9814436fa7b15c6ed5
Noah <noahyasskin@gmail.com>
2016-11-06 02:41:04 +0000

    add css styling for log in and log out

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Users signup" do
     click_link "Sign up"
     fill_in "Email", with: "john@example.com"
     fill_in "Password", with: "Password"
-    fill_in "Password confirmation", with: "password"
+    fill_in "Password confirmation", with: "Password"
     click_button "Sign up"
     expect(page).to have_content("You have signed up successfully")
   end
